0f87c4f5d149ff53891619bbac76613af1a00e7e
NatePillay <nathan94pillay@gmail.com>
2023-01-23 20:20:12 -0800

    new commit

---
+++ 0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - function to sort ints
+ * @array: varaible for all integers to be sorted
+ * @size: the number of integers
+ * Return: a sorted list
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t  i = 0, j = 0;
+	int temp = 0;
+
+	if (array == NULL|| size == 0)
+	{
+		return NULL;
+	}
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
+
+	
